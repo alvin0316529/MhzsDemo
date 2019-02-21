9ae9bb85d59a17879645ef8d8e4ab43f809e55a5
alvin0316529 <2271700484@qq.com>
2019-02-21 10:23:07 +0800

    commit helloworld

---
+++ src/main/java/com/qfh/utils/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.qfh.utils;
+
+/*
+ * @Description
+ * @Author alvin
+ * @Date 2019-02-21 10:20:43
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+    }
+}
