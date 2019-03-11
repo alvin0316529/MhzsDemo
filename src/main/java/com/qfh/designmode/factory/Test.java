d03e0e971d9377209a7f9aeaa8d020bb2642ba2d
alvin0316529 <2271700484@qq.com>
2019-03-11 17:35:23 +0800

    add test

---
+++ src/main/java/com/qfh/designmode/factory/Test.java
@@ -0,0 +1,12 @@
+package com.qfh.designmode.factory;
+
+/*
+ * @Description
+ * @Author alvin
+ * @Date 2019-03-11 17:34:42
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("xx");
+    }
+}
