acfce39f204c50a274e9ddf3e93430fa32f9f086
alvin0316529 <2271700484@qq.com>
2019-03-11 10:04:07 +0800

    add test

--- src/main/java/com/qfh/utils/HelloWorld.java
+++ src/main/java/com/qfh/utils/HelloWorld.java
@@ -8,5 +8,6 @@ package com.qfh.utils;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+        System.out.println("test!!!");
     }
 }
